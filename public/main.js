ebae8f0a6f7fd5a0a3ce85662f37fb424917ba92
Austin <12504656+Austin-Scott@users.noreply.github.com>
2024-04-07 00:58:59 -0500

    test

--- public/main.js
+++ public/main.js
@@ -1,23 +1,24 @@
 
 // Bottom answer buttons
-const showDetailsButton = document.getElementById('showDetailsButton')
-const addToDeckButton = document.getElementById('addToDeckButton')
-const nextSentenceButton = document.getElementById('nextSentenceButton')
+// const showDetailsButton = document.getElementById('showDetailsButton')
+// const addToDeckButton = document.getElementById('addToDeckButton')
+// const nextSentenceButton = document.getElementById('nextSentenceButton')
+
 
 function showAllDetails() {
-    showDetailsButton.classList.add('d-none')
-    addToDeckButton.classList.remove('d-none')
-    nextSentenceButton.classList.remove('d-none')
+    (document.getElementById('showDetailsButton')).classList.add('d-none')
+    (document.getElementById('addToDeckButton')).classList.remove('d-none')
+    (document.getElementById('nextSentenceButton')).classList.remove('d-none')
 }
 
 function addToDeck() {
-    addToDeckButton.classList.add('d-none')
-    nextSentenceButton.classList.add('d-none')
-    showDetailsButton.classList.remove('d-none')
+    (document.getElementById('addToDeckButton')).classList.add('d-none')
+    (document.getElementById('nextSentenceButton')).classList.add('d-none')
+    (document.getElementById('showDetailsButton')).classList.remove('d-none')
 }
 
 function nextSentence() {
-    addToDeckButton.classList.add('d-none')
-    nextSentenceButton.classList.add('d-none')
-    showDetailsButton.classList.remove('d-none')
+    (document.getElementById('addToDeckButton')).classList.add('d-none')
+    (document.getElementById('nextSentenceButton')).classList.add('d-none')
+    (document.getElementById('showDetailsButton')).classList.remove('d-none')
 }
